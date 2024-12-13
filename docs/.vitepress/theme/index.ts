49152ed8c8a2eacdb393f3eda23cacaa733c7a86
fishing886 <145727116+fishlanding@users.noreply.github.com>
2024-12-14 00:34:16 +0800

    提交

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,15 +3,25 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import '@theojs/lumen/theme'
+import { HomeUnderline } from '@theojs/lumen'
+import { DocVideoLink } from '@theojs/lumen'
+import { DocBox, DocLinks, DocBoxCube } from '@theojs/lumen'
 
 export default {
   extends: DefaultTheme,
+  enhanceApp: ({ app }) => {
+    app.component('Home', HomeUnderline)
+    app.component('Vid', DocVideoLink)
+    app.component('Box', DocBox)
+    app.component('Links', DocLinks)
+    app.component('BoxCube', DocBoxCube)
+  },
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
+  // ...
+
 } satisfies Theme
---
+++ docs/src/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "鱼丸 Team"
+  textsuffix: 'Theme'
+  text: "一个致力于MC地图本地化和简体中文翻译的公益团队"
+  tagline: 目前只有一名成员:)
+  actions:
+    - theme: brand
+      text: 看看地图
+      link: /map/
+    - theme: alt
+      text: 看看整合包
+      link: /modpack/
+# features:
+#   - title: Feature A
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#   - title: Feature B
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#   - title: Feature C
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+<Home />
